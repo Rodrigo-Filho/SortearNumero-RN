86f4aa9289a9836dfdaac3cf04eada796819d004
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-08-26 23:11:28 -0300

    Exercicio - Sorteio

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [numero, setNumero] = useState(null);
+
+  const sortearNumero = () => {
+    const numeroAleatorio = Math.floor(Math.random() * 11);
+    setNumero(numeroAleatorio);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>
+        {numero !== null ? `Número sorteado: ${numero}` : 'Nenhum número sorteado'}
+      </Text>
+      <TouchableOpacity style={styles.botao} onPress={sortearNumero}>
+        <Text style={styles.textoBotao}>Sortear</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +24,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  texto: {
+    fontSize: 18,
+    width: 160, // 160dp
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  botao: {
+    backgroundColor: '#800080',
+    padding: 10,
+    borderRadius: 5,
+  },
+  textoBotao: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
